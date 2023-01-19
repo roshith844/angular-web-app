75df065fb5e2057087ee4ed29b10109c827f66eb
Roshith P <roshith844@gmail.com>
2023-01-19 09:59:48 +0530

    feat: user profile image upload

--- web-app/src/app/services/authentication.service.ts
+++ web-app/src/app/services/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { updateProfile } from '@firebase/auth';
-import { from, Observable, switchMap } from 'rxjs';
+import { from, Observable, switchMap, of, concatMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +17,17 @@ export class AuthenticationService {
       return updateProfile(user, {displayName: name})
     }))
   }
+
+    updateProfileData(profileData: Partial<any>): Observable<any> {
+     const user = this.auth.currentUser;
+  return of (user).pipe(
+     concatMap((user) => {
+        if (!user) throw new Error('Not authenticated');
+
+        return updateProfile(user, profileData);
+       })
+    );
+   }
 
   logout(){
     return from(this.auth.signOut())
